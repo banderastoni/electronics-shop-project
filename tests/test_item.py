f8d6d785bb50cef34d6e695f804a1be1bf7d9430
banderastoni <banderastoni@rambler.ru>
2023-07-04 01:00:44 +0300

    homework 1 done

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,20 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item3 = Item("Часы", 30000, 3)
+
+
+def test_init():
+    assert item3.name == "Часы"
+    assert item3.price == 30000
+    assert item3.quantity == 3
+
+
+def test_calculate_total_price():
+    assert item3.calculate_total_price() == 90000
+
+
+def test_apply_discount():
+    Item.pay_rate = 0.5
+    item3.apply_discount()
+    assert item3.price == 15000
