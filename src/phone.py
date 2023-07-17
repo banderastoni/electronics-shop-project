a1b9c1c9072e2b89a81a0510bcb79097b653de89
banderastoni <banderastoni@rambler.ru>
2023-07-17 14:52:46 +0300

    migrate class magic method __add__ from Phone -> Class

--- src/phone.py
+++ src/phone.py
@@ -8,11 +8,6 @@ class Phone(Item):
         super().__init__(name, price, quantity)
         self.__number_of_sim = number_of_sim
 
-    def __add__(self, other):
-        if not isinstance(other, Item):
-            raise ValueError('Складывать можно только объекты Employee и дочерние от них.')
-        return self.quantity + other.quantity
-
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
